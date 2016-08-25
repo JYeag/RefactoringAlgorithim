efc5a07b7ecdcb2c3d3cc6587e051f6dd1b29e3b
Jeremy Yeager <Talif_Moor@yahoo.com>
2016-08-25 13:34:58 -0400

    Actually saved this time

--- Refactoring/CS/Algorithm/Algorithm/Finder.cs
+++ Refactoring/CS/Algorithm/Algorithm/Finder.cs
@@ -6,49 +6,19 @@ namespace Algorithm {
         private readonly List<Person> ListOfPeople;
 
         public Finder(List<Person> people) {
-            ListOfPeople = people;
+            ListOfPeople = people.OrderBy(p => p.BirthDate).ToList();
         }
 
         public Comparison FindDesiredComparison(string option) {
-            var arrangedListOfPeople = ListOfPeople.OrderBy(p => p.BirthDate).ToList();
-            return CheckForBestComparison(arrangedListOfPeople, option);
+            return CheckForBestComparison(ListOfPeople, option);
         }
 
-        public Comparison CheckForBestComparison(List<Person> arrangedListOfPeople, string option) {
+        public Comparison CheckForBestComparison(List<Person> ListOfPeople, string option) {
             var result = new Comparison();
-            if (arrangedListOfPeople.Count > 1) {
-                result = FinderOption.option[option](arrangedListOfPeople);
+            if (ListOfPeople.Count > 1) {
+                result = FinderSearches.option[option](ListOfPeople);
             }
             return result;
         }
-
-        public Comparison FindFurthest(List<Person> arrangedListOfPeople) {
-            return new Comparison {
-                Person1 = arrangedListOfPeople.First(),
-                Person2 = arrangedListOfPeople.Last(),
-                Difference = arrangedListOfPeople.Last().BirthDate - arrangedListOfPeople.First().BirthDate };
-        }
-
-        public Comparison FindClosest(List<Person> arrangedListOfPeople) {
-            List<Comparison> comparisons = MakeListOfClosestPeople(arrangedListOfPeople);
-            return comparisons.OrderBy(c => c.Difference).First();
-        }
-
-        public List<Comparison> MakeListOfClosestPeople(List<Person> arrangedListOfPeople) {
-            List<Comparison> comparisons = new List<Comparison>();
-            for (int i = 0; i < arrangedListOfPeople.Count - 1; i++) {
-                comparisons.Add(MakeComparison(arrangedListOfPeople[i], arrangedListOfPeople[i + 1]));
-            }
-            return comparisons;
-        }
-
-        public Comparison MakeComparison(Person younger, Person older) {
-            return new Comparison {
-                Person1 = younger,
-                Person2 = older,
-                Difference = older.BirthDate - younger.BirthDate
-            };
-        }
-
     }
 }
--- Refactoring/CS/Algorithm/Algorithm/FinderSearches.cs
+++ Refactoring/CS/Algorithm/Algorithm/FinderSearches.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Algorithm
-{
-    static public class FinderOption {
+namespace Algorithm {
+    static public class FinderSearches {
         static public Dictionary<String, Func<List<Person>, Comparison>> option = new Dictionary<String, Func<List<Person>, Comparison>>() {
-            {"Closest", (List<Person> input) =>
-            { return new Comparison(); }
+            {"Closest", (List<Person> ListOfPeople) =>
+                { List<Comparison> comparisons = MakeListOfClosestPeople(ListOfPeople);
+                return comparisons.OrderBy(c => c.Difference).First(); }
             },
-            {"Furthest", (List<Person> input) =>
-            { return new Comparison(); }
+
+            {"Furthest", (List<Person> ListOfPeople) =>
+                { return new Comparison {
+                    Person1 = ListOfPeople.First(),
+                    Person2 = ListOfPeople.Last(),
+                    Difference = ListOfPeople.Last().BirthDate - ListOfPeople.First().BirthDate};}
             }
         };
+
+        static public List<Comparison> MakeListOfClosestPeople(List<Person> ListOfPeople) {
+            List<Comparison> comparisons = new List<Comparison>();
+            for (int i = 0; i < ListOfPeople.Count - 1; i++) {
+                comparisons.Add(MakeComparison(ListOfPeople[i], ListOfPeople[i + 1]));
+            }
+            return comparisons;
+        }
+
+        static public Comparison MakeComparison(Person younger, Person older) {
+            return new Comparison {
+                Person1 = younger,
+                Person2 = older,
+                Difference = older.BirthDate - younger.BirthDate
+            };
+        }
     }
 }
